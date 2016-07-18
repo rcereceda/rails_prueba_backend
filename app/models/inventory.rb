308b45042d6c4eb4f5615084243d0644b24773a8
Rodrigo Cereceda <rcereced@gmail.com>
2016-07-18 20:04:46 -0300

    1 to 1 relations

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,5 +1,6 @@
 class Inventory < ActiveRecord::Base
   belongs_to :category
+  has_one :work
   validates :serial, presence: true, uniqueness: true
   validates :wheelsize, numericality: { greater_than: 0 }
 end
